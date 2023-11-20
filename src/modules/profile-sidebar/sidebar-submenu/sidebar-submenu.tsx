8b8a3bb205719105b5b8b64a98acb63a92359edf
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-11-20 14:42:32 +0300

    начало стилизации субменю

--- src/modules/profile-sidebar/sidebar-submenu/sidebar-submenu.tsx
+++ src/modules/profile-sidebar/sidebar-submenu/sidebar-submenu.tsx
@@ -1,15 +1,20 @@
 import { type FC } from 'react'
 import { type SubmenuItem } from 'src/modules/profile-sidebar/types'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+
+import styles from './index.module.scss'
 
 type SidebarSubmenuProps = {
 	items?: SubmenuItem[]
 }
 export const SidebarSubmenu: FC<SidebarSubmenuProps> = ({ items }) => {
+	const location = useLocation()
+	console.log(location)
+	console.log(items)
 	return (
-		<ul>
+		<ul className={styles.submenu}>
 			{items?.map((subItem) => (
-				<li key={subItem.title}>
+				<li className={subItem.active ? '_active' : ''} key={subItem.title}>
 					<Link to={subItem.link}>{subItem.title}</Link>
 				</li>
 			))}
