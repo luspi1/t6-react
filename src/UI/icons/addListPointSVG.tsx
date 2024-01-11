b5fb0bd9defc29a5f90f57f093a740a7d60686ed
ludaalt <38361378+ludaalt@users.noreply.github.com>
2024-01-11 09:56:06 +0300

    добавлены иконки

---
+++ src/UI/icons/addListPointSVG.tsx
@@ -0,0 +1,20 @@
+export const AddListPointSvg = () => {
+	return (
+		<svg width='17' height='17' viewBox='0 0 17 17' fill='none' xmlns='http://www.w3.org/2000/svg'>
+			<rect
+				width='15'
+				height='0.7'
+				transform='matrix(0 -1 0.999995 0.00305792 8 16)'
+				fill='#113770'
+				stroke='#113770'
+				strokeWidth='0.5'
+			/>
+			<path
+				d='M16.0039 9L1.00391 9L1.00177 8.3L16.0018 8.3L16.0039 9Z'
+				fill='#113770'
+				stroke='#113770'
+				strokeWidth='0.5'
+			/>
+		</svg>
+	)
+}
---
+++ src/UI/icons/deleteCrossSVG.tsx
@@ -0,0 +1,18 @@
+export const DeleteCrossSvg = () => {
+	return (
+		<svg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+			<rect
+				width='21.657'
+				height='0.866282'
+				transform='matrix(0.706025 -0.708187 0.706025 0.708187 0.101562 15.3867)'
+				fill='black'
+			/>
+			<rect
+				width='21.657'
+				height='0.866282'
+				transform='matrix(-0.706025 -0.708187 0.706025 -0.708187 15.2891 15.9507)'
+				fill='black'
+			/>
+		</svg>
+	)
+}
